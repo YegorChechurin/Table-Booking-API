ddd828991048aacb154d0a87d1a945ba2c054835
Yegor Chechurin <yegor.chechurin@gmail.com>
2020-07-20 22:50:55 +0300

    unified format of response messages

--- src/Service/ReservationResponseBuilder.php
+++ src/Service/ReservationResponseBuilder.php
@@ -14,7 +14,7 @@ class ReservationResponseBuilder
     {
         $responseMessage['status'] = '400 Bad Request';
         foreach ($requestValidationErrors as $err) {
-            $responseMessage['invalid_parameter_values'][$err->getPropertyPath()] = $err->getMessage();
+            $responseMessage['message']['invalid_parameter_values'][$err->getPropertyPath()] = $err->getMessage();
         }
 
         return new JsonResponse($responseMessage, 400);
@@ -26,7 +26,7 @@ class ReservationResponseBuilder
         foreach ($conflictingReservations as $conflict) {
             $startTime = $conflict['startTime']->format('Y-m-d H:i');
             $endTime = $conflict['endTime']->format('Y-m-d H:i');
-            $responseMessage['conflicting_reservations']['table '.$conflict['tableId']] = $startTime.' - '.$endTime;
+            $responseMessage['message']['conflicting_reservations']['table '.$conflict['tableId']] = $startTime.' - '.$endTime;
         }
 
         return new JsonResponse($responseMessage, 409);
@@ -35,7 +35,7 @@ class ReservationResponseBuilder
     public function buildOkResponse(ReservationParameters $reservationParams): JsonResponse
     {
         $responseMessage['status'] = '200 OK';
-        $responseMessage['reservation_parameters'] = [
+        $responseMessage['message']['reservation_parameters'] = [
             'table_id' => $reservationParams->getTableId(),
             'from' => $reservationParams->getStartTime(),
             'to' => $reservationParams->getEndTime(),
